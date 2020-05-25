58df1458bf02e82bd3b7d5068936f5b76847a3c9
khuyentran1401 <khuyentran1476@gmail.com>
2020-05-25 16:14:03 -0500

    Add config, gitignore, and change User

--- Project1/project_track/forms.py
+++ Project1/project_track/forms.py
@@ -72,6 +72,41 @@ class Subscribe(forms.Form):
         return email
 
 
+    User._meta.get_field('email')._unique = True
+
+class UserForm(forms.Form):
+    # first_name = forms.CharField(max_length=100)
+    # last_name = forms.CharField(max_length=100)
+    # email = forms.EmailField()
+    # age = forms.IntegerField()
+    # users = forms.CharField(label='User_name',
+    #                         widget=forms.Select(choices=user_list))
+    class Meta:
+        model = User
+        fields = ['user_city','user_facility','user_name','first_name','last_name','email']
+
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['user_name'].queryset = User.objects.none()
+
+class Subscribe(forms.Form):
+    email = forms.EmailField()
+
+    def _str_(self):
+        return self.email
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        # if User.objects.filter(email=email).exists():
+        #     raise forms.ValidationError("This email address is not registered, please sign up first.")
+        return email
+
+
 
 class PassReset(forms.Form):
     # first_name = forms.CharField(max_length=100)
--- Project1/project_track/views.py
+++ Project1/project_track/views.py
@@ -89,12 +89,12 @@ def login(request):
             #                                 user_password=request.POST.get('password'))
             # user = authenticate(request, username=request.POST.get('username'), password=request.POST.get('password'))
             # authenticate(request, email=request.POST.get('email'), user_password=request.POST.get('password'))
-            return render(request, 'login.html', {'form': form, 'message': message})
+
+            return render(request, 'login.html',{'form':form,'message':message})
             # project_track="I am the project_track application"
 
     else:
-        return render(request, 'login.html', {'form': form})
-
+        return render(request, 'login.html',{'form':form})
 
 def signup(request):
 
@@ -188,6 +188,41 @@ def resetpsd(request):
         return render(request, 'reset_password.html', {'form': form})
     return render(request, 'reset_password.html', {'form': form})
 
+def forgetpsd(request):
+    sub = forms.Subscribe()
+    if request.method == 'POST':
+        sub = forms.Subscribe(request.POST)
+        subject = 'Reset your password'
+        message = 'Please use this link to reset your password, http://127.0.0.1:8000/reset'
+        user_email = sub['email'].value()
+        if(User.objects.filter(email=user_email).exists()):
+            #print(User.objects.get(email=user_email))
+            recepient = str(sub['email'].value())
+            message =  message + iri_to_uri(quote('/resetpassword/%s' %quote(recepient)))
+
+            print(message)
+            send_mail(subject, message, settings.EMAIL_HOST_USER,
+                      [recepient], fail_silently = False)
+            return render(request, 'success.html', {'recepient':recepient})
+        else:
+            #print(sub.errors)
+            return render(request, 'forgetpsd.html', {'form': sub, "message":'Your email does not exist. Please sign up first.'})
+    return render(request, 'forgetpsd.html', {'form': sub})
+
+def resetpsd(request):
+    form = forms.PassReset()
+    if request.method == "POST":
+        form = forms.PassReset(request.POST)
+        email = request.POST.get('email')
+        user_password = request.POST.get('user_password')
+        user_password_conf = request.POST.get('user_password_confirm')
+        if user_password == user_password_conf:
+            user_object = User.objects.get(email=email)
+            user_object.user_password = user_password_conf
+            user_object.save()
+            return render(request, 'login.html', {"message":'Password reset, please log in again'})
+        return render(request, 'reset_password.html', {'form': form})
+    return render(request, 'reset_password.html', {'form': form})
 
 def test(request):
     # objects, created=User.objects.get_or_create(user_id=2, user_name = 'archt', first_name = 'arch', last_name = 'talents',
@@ -231,4 +266,5 @@ def test(request):
         context = {}
         # context['form'] = form
         users = User.objects.order_by('user_name')
-    return render(request, 'welcome.html', {'objects': users})
+
+    return render(request, 'welcome.html', {'objects':users})
