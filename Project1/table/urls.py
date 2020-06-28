0eb77cc396903716ce5eae1d9389d6dd7e0f0fdb
Averyzhang761 <59584897+Averyzhang761@users.noreply.github.com>
2020-06-27 21:05:06 -0500

    Update urls.py

--- Project1/table/urls.py
+++ Project1/table/urls.py
@@ -3,9 +3,10 @@ from . import views
 from django.contrib import admin
 
 urlpatterns = [
-    path('', views.index, name="index"),
+#     path('', views.index, name="index"),
     path('new/', views.update_data, name='new'),
     path('table/', views.view_table, name='view_table'),
+    path('..', views.back_to_log_in, name='back_login'),
     path('searchresults/', views.SearchResultsView.as_view(), name='searchresults'),
     path('extension/', views.view_all, name='extension'),
     path('newextension/', views.update_all, name='newextension')
